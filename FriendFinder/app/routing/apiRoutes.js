2840e7e26c068ca9bee60336219fc2c891bcdf4b
Benjamin Owens <Benjamin.Owens@amtrustgroup.com>
2017-07-30 18:41:55 -0400

    works within strict inputs

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -1,35 +1,25 @@
 var express = require("express");
 var app = express.Router();
 
-app.get("/api/friends1", function(req, res) {
-  res.json(friends);
-});
+
 var friends=[];
 app.post("/api/friends", function(req, res) {
 
   var newFriend = req.body;
-  friends.push(newFriend);
-  //console.log(friends.length)
+
+
   res.json(newFriend);
-  //console.log(newFriend.hiking)
-  console.log(friends)
 
-  for(var i=0; i<friends.length; i++){
-  	//console.log(friends[i]);
-var array=[]
-  	var difference = (friends[i].hiking-newFriend.hiking)+(friends[i].parties-newFriend.parties)
-array.push(difference)
-for (vari=0; i<difference.length; i++){array.sort(); console.log(array[0])}
-//return array[0];
+  console.log(newFriend);
+var comparisonArray=[]
+for(var i=0; i<friends.length; i++){
+var difference = Math.abs(friends[i].hiking-newFriend.hiking)+Math.abs(friends[i].parties-newFriend.parties)
+comparisonArray.push(difference)
 
-
-
-  }
-
-//   for(var i=1; i<arr.length; i++){
-//     if(arr[i] < smallest){
-//         smallest = arr[i];   
-//     }
-// }
+var i = comparisonArray.indexOf(Math.min(...comparisonArray));
+console.log (friends)
+console.log ( "You're New Best Friend is " + friends[i].name);
+}
+friends.push(newFriend);
 });
 module.exports = app
